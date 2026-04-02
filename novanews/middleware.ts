fa4d22fe3c1b03eb9484f2181b947054243d2431
Ajay-Sankar-T <ajay4sankar@gmail.com>
2026-04-02 23:10:42 +0530

    Middleware fix

--- novanews/middleware.ts
+++ novanews/middleware.ts
@@ -1,23 +1,19 @@
-// middleware.ts
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import type { NextRequest } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
   "/library(.*)",
   "/api/save(.*)",
 ]);
 
-export default clerkMiddleware(async (auth, req: NextRequest) => {
+export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) {
-    await auth.protect(); // redirects unauthenticated users to Clerk sign-in
+    auth().protect();
   }
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and static files
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
